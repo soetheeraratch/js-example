a0ccb5230678cfa96bd4906800a019fd47183fbc
trax2t <theerarat.so@gmail.com>
2022-07-13 12:39:17 +0700

    test js

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
-import './style.css'
+import JSConfetti from 'js-confetti'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const JsConfetti = new JSConfetti()
+const coin_btn = document.getElementById("btncoin");
+const bill_btn = document.getElementById("btnbill");
+const wallet_img = document.getElementById("wallet");
+const min = Math.ceil(300);
+const max = Math.floor(1500);
+const targetRich = Math.floor(Math.random()*(max-min)+min);
+console.log(`targetRich=${targetRich}`);
+
+function updateMoney(value){
+    const money= document.getElementById('money');
+    const currentValue = parseInt(money.textContent);
+
+    let newValue = currentValue +value;
+    money.textContent = newValue;
+
+    if(newValue > targetRich){
+      jsConfetti.addConfetti()
+    }
+}
+
+coin_btn.addEventListener("click",function() {
+    let iconcoin = `<img class="icon" src="${'assets/coin.png'}" />`
+    wallet_img.insertAdjacentHTML('beforeend',iconcoin);
+    updateMoney(10);
+});
+
+bill_btn.addEventListener('click',function () {
+    let iconbill = `<img class="icon" src="${'assets/bill.png'}" />`
+    wallet_img.insertAdjacentHTML('beforeend',iconbill);
+    updateMoney(100);
+});
